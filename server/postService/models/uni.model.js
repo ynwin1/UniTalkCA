2c87f4c93166984d1ecf37a2dc3138cba6a70942
YanNaingWin1 <ynwin1@student.ubc.ca>
2023-01-22 00:08:44 -0800

    backend config for post connections; APIs created

---
+++ server/postService/models/uni.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const uniPostSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name cannot be empty."]
+    },
+    category: {
+        type: String,
+        required: [true, "Has to be in one Category"]
+    },
+    title: {
+        type: String,
+        required: [true, "Title cannot be empty"]
+    },
+    description: {
+        type: String,
+        required: [true, "Please add the description"]
+    },
+    tags: [String],
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    email: {
+        type: String,
+        required: [true, "An email is required"]
+    }
+})
+
+const UniPost = mongoose.model("UniPost", uniPostSchema);
+module.exports = UniPost;
---
+++ server/postService/routes/uni.route.js
@@ -0,0 +1,20 @@
+const controller = require("../controllers/uni.controller");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  //return all the posts from given university and category
+  app.get(
+    "/api/name/:name/category/:category",
+    controller.getPostByNameAndCategory
+  );
+
+  //make a post
+  app.post("/api/post", controller.createPost);
+};
