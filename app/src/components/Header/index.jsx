60e0279cc18ea61d4cd024434a7637394bdda275
Nehemie <nguimbousnehemie98@yahoo.com>
2023-01-21 18:08:38 -0800

    main page initial

--- app/src/components/Header/index.jsx
+++ app/src/components/Header/index.jsx
@@ -1,11 +1,9 @@
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
+import {useState} from 'react'
 
-
-function Header() {
-   
-	const HomeHeader = styled.header`
+const HomeHeader = styled.header`
 		width: 95%;
 		margin: auto;
 		margin-top: 25px;
@@ -20,26 +18,111 @@ function Header() {
 		height: 50px;
 	`
 	const HeaderNav = styled.nav`
-		width: 20%;
+		width: 10%;
 		height: 30px;
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
 	`
-	const SelectedLink = styled(Link)`
+	const SelectedLink = styled.a`
 		background-color: ${colors.primary};
 		color: white;
 		text-decoration: none;
 		border: 1px solid  ${colors.primary};
 		border-radius:25px;
 	`
-	const HeaderLink = styled(Link)`
-		text-decoration: none;
-		color: ${colors.secondary};
+	const HeaderLink = styled.a`
+	background-color: ${colors.primary};
+	color: white;
+	text-decoration: none;
+	border: 1px solid  ${colors.primary};
+	border-radius:25px;
 	`
 
+	const HomeDescription = styled.div`
+    width: 90%;
+	margin:auto;
+	height:500px;
+    display: flex;
+    flex-direction: column;
+    margin-top: 150px;
+    vertical-align: sup;
+    line-height: 1.5;
+	text-align: center;
+	background-color:${colors.backgroundLight};
+  `
+
+  const SelectedDiv= styled.div`
+	text-align: center;
+	padding-top: 30px;
+`
+const SelectedElement= styled.select`
+width:350px;
+height:40px;
+border-radius: 10px 10px  ;
+`
+const NavOptions= styled.div`
+margin: auto;
+border: 1px solid ${colors.primary};
+border-radius: 10px 10px;
+width:350px;
+
+
+`
+const LinkOptions= styled.a`
+color :${colors.secondary};
+display: inline-block;
+width:250px;
+height:100px;
+line-height:100px;
+`
+
+
+function Header() {
+   
+	const [isSelected, setIsSelected] = useState (false);
+
 	return (
-		<h1> Our Header</h1>
+		<div>
+		<HomeHeader >
+			<HeaderLogo src="" alt="logo" />
+			<HeaderNav>
+				<HeaderLink > &nbsp;&nbsp;Login &nbsp;&nbsp;</HeaderLink>
+				<HeaderLink > &nbsp;&nbsp;SignIn &nbsp;&nbsp;</HeaderLink>
+				
+			</HeaderNav>
+		</HomeHeader>
+
+<HomeDescription>
+	<h1> Canadian Universities </h1>
+	
+
+<SelectedDiv>
+<SelectedElement  >
+    <option value="none">Select a University</option>
+	<option value="UBC">UBC</option>
+	<option  value="SFU">SFU</option>
+	<option  value="UVIC">UVIC</option>
+	<option  value="TRU">TRU</option>
+	<option  value="Waterloo">Waterloo</option>
+	
+ </SelectedElement> 
+ 
+</SelectedDiv>
+<NavOptions>
+ <LinkOptions>Admissions </LinkOptions>
+ </NavOptions>
+ 
+
+ <NavOptions>
+ <LinkOptions>General </LinkOptions>
+ </NavOptions>
+
+ <NavOptions>
+ <LinkOptions> Forum </LinkOptions>
+ </NavOptions>
+	</HomeDescription>
+	</div>
 	)
 }
 
